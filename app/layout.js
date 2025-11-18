630d5ee6bc6198fa4675038954135427ec54c3c6
GuštinGal <70420472+Galicius@users.noreply.github.com>
2025-11-18 12:16:37 +0100

    hotjar

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,16 @@
-'use client';
+"use client";
 
-import './globals.css';
-import { AuthProvider } from '@/lib/auth';
+import "./globals.css";
+import { AuthProvider } from "@/lib/auth";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://t.contentsquare.net/uxa/5ce2d4ab93a5d.js"></script>
+      </head>
       <body>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
